2b7ebd729f6646af60f90129d90a0501e7aea73d
Samtmeg <160788397+Samtmeg@users.noreply.github.com>
2024-05-10 18:32:40 -0400

    updated Header and Footer sections

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import './footer.css'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 function Footer () {
     return (
-        <div>
-            
+        <div className="footer-container">
+            <div className="footer-social-media-sec">
+                <div><FacebookIcon/></div>
+                <div><InstagramIcon/></div>
+                <div><XIcon/></div>
+                <div><YouTubeIcon/></div>
+            </div>
+            <div className="footer-main-grid-container">
+                <div class="grid-item">Audio Description</div>
+                <div class="grid-item">Help centre</div>
+                <div class="grid-item">Gift Cards</div>
+                <div class="grid-item">Media Centre</div>
+                <div class="grid-item">Investor Relations</div>
+                <div class="grid-item">Jobs</div>
+                <div class="grid-item">Netflix Shop</div>
+                <div class="grid-item">Terms of Use</div>
+                <div class="grid-item">Privacy</div>
+                <div class="grid-item">Legal Notices</div>
+                <div class="grid-item">Cookie Preferences</div>
+                <div class="grid-item">Corporate Information</div>
+                <div class="grid-item">Contact Us</div>
+                <div class="grid-item">Advert choices</div>
+            </div>
+            <div className="footer-servicecode-sec"><div>Service Code</div></div>
+            <div className="footer-copyright-sec"> © 1997 - 2024 Netflix, Inc</div>
         </div>
     );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
+import './header.css';
+import NetflixLogo from '../../assets/NetflixLogo.png';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 function Header () {
     return (
-        <div>
-
-        </div>
+        <>
+            <navbar>
+                <div className="left-navigation">
+                    <img src={NetflixLogo} alt="" height={50} />
+                    <ul>
+                        <li>Home</li>
+                        <li>Series</li>
+                        <li>Films</li>
+                        <li>New & Popular</li>
+                        <li>My List</li>
+                        <li>Browse by Languages</li>
+                    </ul>
+                </div>
+                <div className='right-navigation'>
+                        <div><SearchIcon/></div>
+                        <div><NotificationsOutlinedIcon /></div>
+                        <div><AccountBoxOutlinedIcon/></div>
+                        <div><ArrowDropDownIcon/></div>
+                </div>
+            </navbar>
+        </>
     );
 }
 
