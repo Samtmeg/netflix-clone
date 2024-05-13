011ff2d657c82e0551c976f6e31247e56994e7ed
Samtmeg <160788397+Samtmeg@users.noreply.github.com>
2024-05-13 04:04:40 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "/samtmeg/netflix-clone", plugins: [react()],
 })
+
